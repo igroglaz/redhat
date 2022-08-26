44684c3e43b9aa41b09c07af922bc648f58396af
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-08-26 22:58:43 +0300

    simplify stat threshold for entering server

--- client.cpp
+++ client.cpp
@@ -1548,17 +1548,11 @@ bool CL_EnterServer(Client* conn, Packet& pack)
                     exp_total += chrtc.ExpEarthPike;
                     exp_total += chrtc.ExpAstralShooting;
 
-                    // 6+ server flag (due most often check which is used for all high tiers maps)
-                    bool too_weak_for_high_maps = 0;
-                    if ((chrtc.Body < 43 || chrtc.Reaction < 43 || chrtc.Mind < 43 || chrtc.Spirit < 43) ||
-                         exp_total < 1)
-                            too_weak_for_high_maps = 1;
-
                     // flag to check if char drinked all stat pots at this particular server
                     bool no_enter_server = 0;
 
+                    // it takes 'pure' stats, without boni from items
                     if      (srv->Number == 1 &&
-                            // it takes 'pure' stats, without boni from items
                             (chrtc.Body > 15 || chrtc.Reaction > 15 || chrtc.Mind > 15 || chrtc.Spirit > 1))
                                 no_enter_server = 1;
                     else if (srv->Number == 2 &&
@@ -1567,61 +1561,58 @@ bool CL_EnterServer(Client* conn, Packet& pack)
                                 no_enter_server = 1;
                     else if (srv->Number == 3 &&
                             ((chrtc.Body > 25 && chrtc.Reaction > 25 && chrtc.Mind > 25 && chrtc.Spirit > 25) ||
-                             chrtc.Spirit < 15 ||
                              exp_total < 1))
                                 no_enter_server = 1;
                     else if (srv->Number == 4 &&
                             ((chrtc.Body > 34 && chrtc.Reaction > 34 && chrtc.Mind > 34 && chrtc.Spirit > 34) ||
-                             (chrtc.Body < 25 || chrtc.Reaction < 25 || chrtc.Mind < 25 || chrtc.Spirit < 25) ||
                               exp_total < 1))
                                 no_enter_server = 1;
                     else if (srv->Number == 5 &&
                             ((chrtc.Body > 43 && chrtc.Reaction > 43 && chrtc.Mind > 43 && chrtc.Spirit > 43) ||
-                             (chrtc.Body < 34 || chrtc.Reaction < 34 || chrtc.Mind < 34 || chrtc.Spirit < 34) ||
                               exp_total < 1))
                                 no_enter_server = 1;
-                    else if (srv->Number == 6 && (too_weak_for_high_maps ||
+                    else if (srv->Number == 6 && (exp_total < 1 ||
                             (chrtc.Body > 52 && chrtc.Reaction > 52 && chrtc.Mind > 52 && chrtc.Spirit > 43)))
                                 no_enter_server = 1;
-                    else if (srv->Number == 7 && (too_weak_for_high_maps ||
+                    else if (srv->Number == 7 && (exp_total < 1 ||
                             (chrtc.Body > 52 && chrtc.Reaction > 52 && chrtc.Mind > 52 && chrtc.Spirit > 52)))
                                 no_enter_server = 1;
                     else if (srv->Number == 8)
                     {
                         if (chrtc.Sex == 0 &&       // warrior
                            ((chrtc.Body > 54 && chrtc.Reaction > 52 && chrtc.Mind > 62 && chrtc.Spirit > 52) ||
-                             too_weak_for_high_maps))
+                             exp_total < 1))
                                 no_enter_server = 1;
                         else if (chrtc.Sex == 64 && // mage
                           ((chrtc.Body > 53 && chrtc.Reaction > 52 && chrtc.Mind > 62 && chrtc.Spirit > 52) ||
-                            too_weak_for_high_maps))
+                            exp_total < 1))
                                 no_enter_server = 1;
                         else if (chrtc.Sex == 128 && // amazon
                           ((chrtc.Body > 52 && chrtc.Reaction > 62 && chrtc.Mind > 52 && chrtc.Spirit > 52) ||
-                            too_weak_for_high_maps))
+                            exp_total < 1))
                                 no_enter_server = 1;
                         else if (chrtc.Sex == 192 && // witch
                           ((chrtc.Body > 52 && chrtc.Reaction > 62 && chrtc.Mind > 52 && chrtc.Spirit > 52) ||
-                            too_weak_for_high_maps))
+                            exp_total < 1))
                                 no_enter_server = 1;
                     }
                     else if (srv->Number == 9)
                     {
                         if (chrtc.Sex == 0 &&       // warrior
                            ((chrtc.Body > 55 && chrtc.Reaction > 52 && chrtc.Mind > 76 && chrtc.Spirit > 76) ||
-                             too_weak_for_high_maps))
+                             exp_total < 1))
                                 no_enter_server = 1;
                         else if (chrtc.Sex == 64 && // mage
                            ((chrtc.Body > 54 && chrtc.Reaction > 76 && chrtc.Mind > 62 && chrtc.Spirit > 62) ||
-                             too_weak_for_high_maps))
+                             exp_total < 1))
                                 no_enter_server = 1;
                         else if (chrtc.Sex == 128 && // amazon
                            ((chrtc.Body > 52 && chrtc.Reaction > 76 && chrtc.Mind > 52 && chrtc.Spirit > 52) ||
-                             too_weak_for_high_maps))
+                             exp_total < 1))
                                 no_enter_server = 1;
                         else if (chrtc.Sex == 192 && // witch
                            ((chrtc.Body > 52 && chrtc.Reaction > 76 && chrtc.Mind > 52 && chrtc.Spirit > 62) ||
-                             too_weak_for_high_maps))
+                             exp_total < 1))
                                 no_enter_server = 1;
                     }
 
